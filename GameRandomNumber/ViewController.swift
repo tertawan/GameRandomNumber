7f9ea41b1945ef463dd3cad6575b4ed284394a2a
Ninx <xasnaxxasnax@gmail.com>
2019-04-11 01:02:33 +0700

    Initial Commit

---
+++ GameRandomNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameRandomNumber
+//
+//  Created by Ninxi on 11/4/2562 BE.
+//  Copyright © 2562 Ninxxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
